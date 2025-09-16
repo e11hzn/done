4659825359cc38c02e4c7906dc78c4775ced9a13
e11hzn <haitham.zaben@elva11.se>
2025-09-16 11:43:02 +0200

    feat: initial sidebar component

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { motion } from 'motion/react';
+import { IconButton } from '../IconButton';
+import { CloseIcon } from '@/icons/CloseIcon';
+
+export type SidebarProps = {
+  children: React.ReactNode;
+  onClose: () => void;
+  show: boolean;
+};
+
+export const Sidebar = ({ children, onClose, show }: SidebarProps) => {
+  return (
+    <>
+      <motion.div
+        animate={
+          show
+            ? { display: 'block', opacity: 1 }
+            : { display: 'none', opacity: 0 }
+        }
+        className="bg-black/70 h-screen w-screen fixed left-0 top-0 m-0 select-none hidden"
+        onTap={onClose}
+        transition={{ duration: 0.3 }}
+      />
+      <motion.div
+        animate={show ? { right: 0 } : { right: '-20rem' }}
+        className="fixed top-0 right-[-20rem] w-80 max-w-full h-screen bg-white shadow-[16px_0_32px_-16px_#000] p-4 z-10 flex flex-col gap-4"
+        transition={{ duration: 0.3 }}
+      >
+        <div className="text-2xl text-orange-900 pb-2 border-b-2 border-b-gray-200 flex justify-between items-center">
+          <span>Filter and Sort</span>
+          <IconButton icon={CloseIcon} onClick={onClose} />
+        </div>
+        {children}
+      </motion.div>
+    </>
+  );
+};
--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -8,6 +8,9 @@ import { PencilIcon } from '@/icons/PencilIcon';
 import { CheckIcon } from '@/icons/CheckIcon';
 import { PlusIcon } from '@/icons/PlusIcon';
 import { AlertIcon } from '@/icons/AlertIcon';
+import { Sidebar } from '@/components/Sidebar/Sidebar';
+import { useState } from 'react';
+import { FilterIcon } from '@/icons/FilterIcon';
 
 export const TodosList = () => {
   const {
@@ -19,6 +22,7 @@ export const TodosList = () => {
     t,
     todos,
   } = useAppContext();
+  const [showSidebar, setShowSidebar] = useState(false);
 
   const onDeleteTodo = (id: number) => {
     setTodos(todos.filter((todo) => todo.id !== id));
@@ -38,146 +42,156 @@ export const TodosList = () => {
   };
 
   return (
-    <div className="w-full">
-      <AnimatePresence>
-        {todos.length === 0 ? (
-          <motion.p
-            animate={{ opacity: 1, height: 'auto' }}
-            className="text-amber-600"
-            exit={{ opacity: 0, height: 0 }}
-            key="no-todos"
-            transition={{ duration: 0.5 }}
-          >
-            {t.todosList.emptyList}
-          </motion.p>
-        ) : (
-          <motion.div
-            animate={{ opacity: 1, height: 'auto' }}
-            exit={{ opacity: 0, height: 0 }}
-            key="has-todos"
-            transition={{ duration: 0.5 }}
-          >
-            <div className="flex justify-between">
-              <h2 className="text-2xl text-orange-900 mb-4">
-                {t.todosList.title}
-              </h2>
-              {!createButtonClicked && (
-                <IconButton
-                  className="h-8 w-8"
-                  icon={PlusIcon}
-                  onClick={setCreateButtonClicked}
-                />
-              )}
-            </div>
-            <div className="flex flex-col gap-6">
-              <AnimatePresence>
-                {todos.map((item) => {
-                  let datePassed = false;
-                  let withinOneDay = false;
-                  if (!item.done && item.date) {
-                    const currentTime = new Date().getTime();
-                    const itemTime = new Date(item.date as string).getTime();
-                    datePassed = currentTime > itemTime;
-                    withinOneDay = itemTime - currentTime < 24 * 3600 * 1000;
-                  }
+    <>
+      <div className="w-full">
+        <AnimatePresence>
+          {todos.length === 0 ? (
+            <motion.p
+              animate={{ opacity: 1, height: 'auto' }}
+              className="text-amber-600"
+              exit={{ opacity: 0, height: 0 }}
+              key="no-todos"
+              transition={{ duration: 0.5 }}
+            >
+              {t.todosList.emptyList}
+            </motion.p>
+          ) : (
+            <motion.div
+              animate={{ opacity: 1, height: 'auto' }}
+              exit={{ opacity: 0, height: 0 }}
+              key="has-todos"
+              transition={{ duration: 0.5 }}
+            >
+              <div className="flex justify-between">
+                <h2 className="text-2xl text-orange-900 mb-4">
+                  {t.todosList.title}
+                </h2>
+                <div className="flex gap-4">
+                  <IconButton
+                    icon={FilterIcon}
+                    onClick={() => setShowSidebar(true)}
+                  />
+                  {!createButtonClicked && (
+                    <IconButton
+                      icon={PlusIcon}
+                      onClick={setCreateButtonClicked}
+                    />
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-col gap-6">
+                <AnimatePresence>
+                  {todos.map((item) => {
+                    let datePassed = false;
+                    let withinOneDay = false;
+                    if (!item.done && item.date) {
+                      const currentTime = new Date().getTime();
+                      const itemTime = new Date(item.date as string).getTime();
+                      datePassed = currentTime > itemTime;
+                      withinOneDay = itemTime - currentTime < 24 * 3600 * 1000;
+                    }
 
-                  return (
-                    <motion.div
-                      animate={{ opacity: 1, height: 'auto' }}
-                      className="flex flex-col w-full border-gray-200 border-2 p-2 rounded gap-4 relative"
-                      exit={{ opacity: 0, height: 0 }}
-                      initial={{ opacity: 0, height: 0 }}
-                      key={item.id}
-                      transition={{ duration: 0.5 }}
-                    >
-                      <div className="flex gap-4 items-center">
-                        <h3 className="text-2xl font-bold">{item.name}</h3>
-                        <div className="flex justify-end gap-4 flex-1">
-                          <IconButton
-                            icon={PencilIcon}
-                            onClick={() => setEditTodo(item)}
-                          />
-                          <IconButton
-                            icon={DeleteIcon}
-                            onClick={() => onDeleteTodo(item.id)}
-                          />
+                    return (
+                      <motion.div
+                        animate={{ opacity: 1, height: 'auto' }}
+                        className="flex flex-col w-full border-gray-200 border-2 p-2 rounded gap-4 relative"
+                        exit={{ opacity: 0, height: 0 }}
+                        initial={{ opacity: 0, height: 0 }}
+                        key={item.id}
+                        transition={{ duration: 0.5 }}
+                      >
+                        <div className="flex gap-4 items-center">
+                          <h3 className="text-2xl font-bold">{item.name}</h3>
+                          <div className="flex justify-end gap-4 flex-1">
+                            <IconButton
+                              icon={PencilIcon}
+                              onClick={() => setEditTodo(item)}
+                            />
+                            <IconButton
+                              icon={DeleteIcon}
+                              onClick={() => onDeleteTodo(item.id)}
+                            />
+                          </div>
                         </div>
-                      </div>
-                      {item.date && (
-                        <div className="flex flex-col">
-                          <span className="italic">
-                            {t.todosList.dueDate}:&nbsp;
-                            {new Date(item.date as string).toLocaleString(
-                              locale,
-                            )}
-                          </span>
-                          <span className="italic">
-                            {datePassed ? (
-                              <div className="flex">
-                                <AlertIcon fill="red" />
-                                <span className="ml-2">
-                                  {t.todosList.datePassed}
-                                </span>
-                              </div>
-                            ) : (
-                              withinOneDay && (
+                        {item.date && (
+                          <div className="flex flex-col">
+                            <span className="italic">
+                              {t.todosList.dueDate}:&nbsp;
+                              {new Date(item.date as string).toLocaleString(
+                                locale,
+                              )}
+                            </span>
+                            <span className="italic">
+                              {datePassed ? (
                                 <div className="flex">
-                                  <AlertIcon fill="orange" />
+                                  <AlertIcon fill="red" />
                                   <span className="ml-2">
-                                    {t.todosList.withinOneDay}
+                                    {t.todosList.datePassed}
                                   </span>
                                 </div>
-                              )
-                            )}
-                          </span>
-                        </div>
-                      )}
-                      {item.description && (
-                        <span
-                          dangerouslySetInnerHTML={{
-                            __html: item.description.replace(
-                              /(?:\r\n|\r|\n)/g,
-                              '<br>',
-                            ),
-                          }}
-                        />
-                      )}
-                      {item.categories.length !== 0 && (
-                        <div className="flex flex-wrap gap-2">
-                          {item.categories.map((category) => (
-                            <div
-                              key={category}
-                              className="border-gray-200 border-2 rounded p-1"
-                            >
-                              {category}
-                            </div>
-                          ))}
-                        </div>
-                      )}
-                      <div className="flex gap-4 justify-between items-center border-t-2 border-gray-200 pt-2">
-                        <div>
-                          <span>{`${t.todosList.done}: `}</span>
+                              ) : (
+                                withinOneDay && (
+                                  <div className="flex">
+                                    <AlertIcon fill="orange" />
+                                    <span className="ml-2">
+                                      {t.todosList.withinOneDay}
+                                    </span>
+                                  </div>
+                                )
+                              )}
+                            </span>
+                          </div>
+                        )}
+                        {item.description && (
                           <span
-                            className={
-                              item.done ? 'text-green-600' : 'text-red-700'
-                            }
-                          >
-                            {item.done ? t.todosList.yes : t.todosList.no}
-                          </span>
+                            dangerouslySetInnerHTML={{
+                              __html: item.description.replace(
+                                /(?:\r\n|\r|\n)/g,
+                                '<br>',
+                              ),
+                            }}
+                          />
+                        )}
+                        {item.categories.length !== 0 && (
+                          <div className="flex flex-wrap gap-2">
+                            {item.categories.map((category) => (
+                              <div
+                                key={category}
+                                className="border-gray-200 border-2 rounded p-1"
+                              >
+                                {category}
+                              </div>
+                            ))}
+                          </div>
+                        )}
+                        <div className="flex gap-4 justify-between items-center border-t-2 border-gray-200 pt-2">
+                          <div>
+                            <span>{`${t.todosList.done}: `}</span>
+                            <span
+                              className={
+                                item.done ? 'text-green-600' : 'text-red-700'
+                              }
+                            >
+                              {item.done ? t.todosList.yes : t.todosList.no}
+                            </span>
+                          </div>
+                          <IconButton
+                            icon={CheckIcon}
+                            onClick={() => onTodoDone(item.id)}
+                          />
                         </div>
-                        <IconButton
-                          icon={CheckIcon}
-                          onClick={() => onTodoDone(item.id)}
-                        />
-                      </div>
-                    </motion.div>
-                  );
-                })}
-              </AnimatePresence>
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
+                      </motion.div>
+                    );
+                  })}
+                </AnimatePresence>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      <Sidebar onClose={() => setShowSidebar(false)} show={showSidebar}>
+        <div>hej</div>
+      </Sidebar>
+    </>
   );
 };
